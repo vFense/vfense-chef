b08293bb7ef3b4f960130d32dbc650c60bb4c9f8
Allen Sanabria <asanabria@linuxdynasty.org>
2015-03-13 22:27:00 -0400

    Chagne the name attribute

--- recipes/_ubuntu_dependencies.rb
+++ recipes/_ubuntu_dependencies.rb
@@ -1,6 +1,3 @@
-include_recipe 'apt::default'
-include_recipe 'vfense::_nginx_ppa'
-include_recipe 'vfense::_redis_ppa'
 
 node['vfense']['server']['dependencies']['ubuntu'].each do |pkg|
   package pkg do
@@ -8,10 +5,10 @@ node['vfense']['server']['dependencies']['ubuntu'].each do |pkg|
   end
 end
 
-service "redis-server" do
-  supports :status => true, :start => true, :stop => true, :restart => true
-  action [:enable, :start]
-end
+include_recipe 'apt'
+include_recipe 'vfense::_nginx_ppa'
+include_recipe 'vfense::_redis_ppa'
+
 
 include_recipe "vfense::_redis"
 include_recipe "vfense::_nginx"
